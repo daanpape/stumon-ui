c75c74bc68c5cd22d66d4d3177a6b26cde9152c2
Daan Pape <daan@dptechnics.com>
2016-06-13 17:12:33 +0200

    updated login page

---
+++ js/pages/login.js
@@ -0,0 +1,52 @@
+/**
+ * HoWest StuMON - login.js 
+ * (C) 2016 - Daan Pape
+ * License: MIT
+ */
+
+/**
+ * The Knockout viewmodel for the login page.
+ * @returns {LoginViewModel} a new View model.
+ */
+function LoginViewModel() {
+    var self = this;
+    
+    this.username = ko.observable();
+    this.password = ko.observable();
+    
+    /**
+     * Try to login the user with the credentials in
+     * the username and password variables.
+     */
+    this.login = function() {
+        $.ajax({
+            type: "POST",
+            url: "userapi/v1/login",
+            dataType: 'json',
+            data: { username : self.username(), password : self.password()},
+            success: function() {
+                window.location.href("index.html");
+            },
+            error: function () {
+                BootstrapDialog.show({
+                    type: BootstrapDialog.TYPE_DANGER,
+                    title: "Kon niet aanmelden",
+                    message: "De gebruikersnaam en wachtwoord combinatie zijn niet correct.",
+                    buttons: [{
+                        label: 'Ok',
+                        action: function(dialog) {
+                            dialog.close();
+                        }
+                    }]
+                });
+            }
+        });    
+    };
+}
+
+/**
+ * Page initialisation handler.
+ */
+$('document').ready(function() {
+    ko.applyBindings(new LoginViewModel());
+});
