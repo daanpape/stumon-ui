5ce498c79c503f87d8d037b28106dcf593a92822
Daan Pape <daan@dptechnics.com>
2016-06-13 23:09:20 +0200

    update online scanner list

--- js/pages/index.js
+++ js/pages/index.js
@@ -10,11 +10,25 @@
  */
 function IndexViewModel() {
     var self = this;
+    
+    this.scanners = ko.observableArray([]);
+    
+    /**
+     * Update the online scanner list.
+     */
+    this.updateOnlineScaners = function() {
+        $.getJSON('userapi/scanners', function(data) {
+            self.scanners(data);
+        });
+    };
 }
 
 /**
  * Page initialisation handler.
  */
 $('document').ready(function() {
-    ko.applyBindings(new IndexViewModel());
+    var viewModel = new IndexViewModel();
+    ko.applyBindings(viewModel);
+    
+    setInterval(viewModel.updateOnlineScaners, 1000);
 });
